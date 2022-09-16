a2f3826a1d7fda17eb0a1488ffac074ad700a995
meskyA <mesky2015@gmail.com>
2022-09-16 12:51:03 -0700

    changed schema file

--- db/schema.sql
+++ db/schema.sql
@@ -4,43 +4,25 @@ CREATE DATABASE employees;
 USE employees;
 
 CREATE TABLE department (
-
 id INT NOT NULL AUTO_INCREMENT,
-
 name VARCHAR(30) NOT NULL,
-
 PRIMARY KEY(id)
-
 );
 
 CREATE TABLE role (
-
 id INT NOT NULL AUTO_INCREMENT,
-
 title VARCHAR(30) NOT NULL,
-
 salary DECIMAL(10,2) NOT NULL,
-
 department_id INT NOT NULL,
-
 PRIMARY KEY (id)
-
 );
 
 CREATE TABLE employee (
-
 id INT NOT NULL AUTO_INCREMENT,
-
 first_name VARCHAR(30) NOT NULL,
-
 last_name VARCHAR(30) NOT NULL,
-
 role_id INT NOT NULL,
-
 manager_id INT,
-
 PRIMARY KEY (id)
-
 );
-use employees;
 
